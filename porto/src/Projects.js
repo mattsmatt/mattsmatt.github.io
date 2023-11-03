aa6725837bec8476f9d648d50b6bd0fd37b9a049
Matthew Avrillio <matthew.avrillio@binus.ac.id>
2023-11-03 18:09:04 +0700

    Commit changes

--- porto/src/Projects.js
+++ porto/src/Projects.js
@@ -1,18 +1,17 @@
 import './App.css';
+import Card from './Card.js';
 
 function Projects(){
     return(
-        <section id='projects' className='flex justify-center my-20 text-color-navbar'>
-            <div className='w-[80%] text-left'>
-                <div>Hello! My name is</div>
-                <div>Matthew Avrillio</div>
-                <div>I am a Software Engineer</div>
-                <p>
-                Currently, I am an undergraduate student majoring in Computer Science in Bina Nusantara University. I am in my third year, 5th semester specifically, and I am taking the Software Engineering streaming program. I enjoy problem-solving while working with people in a team environment. I am a fast learner and I am competent in working under pressure.</p>
-
-                <p>Right now, I am exploring JavaFX and integrating Java with MySQL. I am familiar in HTML, CSS, Java, Python, C, ASP.NET and C#. I am also capable of database querying using MySQL and UI/UX design with Figma.</p>
+        <section className='my-20 text-color-navbar'>
+            <div className='text-[2rem] font-semibold my-5'>Projects</div>
+            <div id='projects' className='flex justify-center'>
+                <div className='w-[80%] flex justify-between items-center flex-wrap'>
+                    <Card />
+                </div>
             </div>
         </section>
+        
     );
 }
 
